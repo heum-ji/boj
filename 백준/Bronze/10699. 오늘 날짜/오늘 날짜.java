599825dd003c03df48346b65a52f32140f1845c0
Heum <dragon2009t@gmail.com>
2023-01-21 20:10:15 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 13388 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,10 +1,13 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd"); // 출력 포맷 'YYYY-MM-DD'
 
-        System.out.println(simpleDateFormat.format(new Date()));
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul")); // Timezone '서울'
+
+        System.out.println(simpleDateFormat.format(new Date())); // 서울의 오늘 날짜를 "YYYY-MM-DD" 형식으로 출력한다.
     }
 }
