15209cad95f7dc7780f9199eab487e2e0e8c9733
Jiheum Baek <dragon2009t@gmail.com>
2023-01-26 01:08:10 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -9,18 +9,16 @@ public class Main {
 
         // [입력] 첫째 줄에 정수 N(0 ≤ N ≤ 12) : 479,001,600 (최대값) int 범위
         int inputN = Integer.parseInt(br.readLine());
+        br.close();
+
+        int result = 1;
+
+        // for문으로 풀이
+        for (int i = inputN; i > 1; i--) {
+            result *= i;
+        }
 
         // [출력] 첫째 줄에 N!을 출력한다.
-        System.out.println(factorial(inputN)); // 팩토리얼 호출
-
-        br.close();
-    }
-
-    // 재귀 방법으로 풀이
-    private static int factorial(int n) {
-        if (n <= 1) { // 0! = 1
-            return 1;
-        }
-        return n * factorial(n - 1);
+        System.out.println(result); // for문 출력
     }
 }
