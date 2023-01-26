0b4d4b4b24950cb7ce61f7ab1dd0f3e8b54b2a4d
Jiheum Baek <dragon2009t@gmail.com>
2023-01-26 23:30:47 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -10,16 +10,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringTokenizer st;
+        int inputA;
+        int inputB;
+            
         // [처리]
         while (true) {
             /*
              * [입력] 입력은 여러 개의 테스트 케이스로 이루어져 있다.
              * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
              */
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int inputA = Integer.parseInt(st.nextToken());
-            int inputB = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            inputA = Integer.parseInt(st.nextToken());
+            inputB = Integer.parseInt(st.nextToken());
 
             // 입력의 마지막에는 0 두 개가 들어온다.
             if (inputA == 0 && inputB == 0) {
