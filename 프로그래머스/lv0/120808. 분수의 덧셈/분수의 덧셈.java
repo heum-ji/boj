1ddbd7c3c0252e8cb8ecb3e727f59bd5bd8bb748
Jiheum Baek <dragon2009t@gmail.com>
2023-01-30 00:32:56 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.26 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+        
+        // 분수 통분
+        int numer3 = denom2 * numer1 + denom1 * numer2; // 분자
+        int denom3 = denom1 * denom2; // 분모
+        int maxNum = 1; // 최대공약수
+        
+        // 분모를 분자로 나눌 수 있는 경우
+//         if (denom3 % numer3 == 0) {
+//             denom3 /= numer3;
+            
+//             answer[0] = denom3;
+//             answer[1] = 1;
+            
+//             return answer;
+//         }
+        
+        // 최대공약수 구하기 (기약 분수)
+        for(int i = 1; i <= numer3; i++) {
+            // 둘 다 나누어 지는 경우
+            if(numer3 % i == 0 && denom3 % i == 0) {
+                maxNum = i;
+            }
+        }
+        
+        answer[0] = numer3 / maxNum;
+        answer[1] = denom3 / maxNum;
+        
+        return answer;
+    }
+}
