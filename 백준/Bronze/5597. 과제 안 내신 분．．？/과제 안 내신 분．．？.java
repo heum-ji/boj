c6bc884cce2ab7548d891ede9e35aafe842ec6ea
Jiheum Baek <dragon2009t@gmail.com>
2023-01-29 16:07:10 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
 public class Main {
 
@@ -13,23 +12,20 @@ public class Main {
 
         // 입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 
         // 출석번호에 중복은 없다.
-        ArrayList<Integer> arrList = new ArrayList<Integer>(30); // 학생 30명
-
-        // 정렬된 1 ~ 30 삽입
-        for (int i = 0; i < 30; i++) {
-            arrList.add(i + 1);
-        }
+        boolean[] student = new boolean[30];
 
         // 제출한 학생 제거
-        for (int i = 0; i < 28; i++) {
-            String curVal = br.readLine();
-            arrList.removeIf(n -> n == Integer.parseInt(curVal));
+        for (byte i = 0; i < 28; i++) {
+            student[Byte.parseByte(br.readLine()) - 1] = true;
         }
 
-        // 제출 안 한 학생 출력 - 정렬되어 있음
-        for (Integer student : arrList) {
-            bw.write(String.valueOf(student));
-            bw.newLine();
+        // 제출 안 한 학생 출력
+        for (int i = 0; i < 30; i++) {
+            // 제출 안 한 학생 = false
+            if (!student[i]) {
+                bw.write(String.valueOf(i + 1)); // 학생은 1부터 시작
+                bw.newLine();
+            }
         }
         bw.flush();
         bw.close();
